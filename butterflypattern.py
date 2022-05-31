224971999b6b38a5baabdf1c7b4ed35edd738fc9
PAVAN VARSHNEY <91428581+Pavanvarshney@users.noreply.github.com>
2022-05-31 15:22:38 +0530

    butterflypattern

---
+++ butterflypattern.py
@@ -0,0 +1,19 @@
+rows = 5
+for i in range(1,rows + 1):
+    for j in range(i):
+        print('*',end = ' ')
+    for j in range(rows - i):
+        print(' ',end = ' ')
+    for j in range(rows - i):
+        print(' ',end = ' ')
+    for j in range(i):
+        print('*',end = ' ')
+for j in range(rows - i):
+    print('*',end = ' ')
+for j in range(i):
+    print(' ',end = ' ')
+for j in range(i):
+    print(' ',end = ' ')
+for j in range(rows - i):
+    print('*',end = ' ')
+    print()
